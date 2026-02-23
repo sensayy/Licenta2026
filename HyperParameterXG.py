111355827f17853231598763872d7d1d516bc35f
Abel Mihai <alex2andru55@gmail.com>
2026-02-23 21:03:32 +0200

    Refactor hyperparameter tuning to use RandomizedSearchCV and add model evaluation metrics; include new confusion matrix and importance graphs for XGBoost and Random Forest

--- HyperParameterXG.py
+++ HyperParameterXG.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+import cupy as cp
 
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, ConfusionMatrixDisplay
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,7 +22,9 @@ if y.min() == 1: #XGBoost foloseste clase DOAR incepand cu 0 deci modificam
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y) #Facem train test split, stratify face ca splitul de 80/20 sa fie EGAL pentru toate clasele
 
+
 xgb = XGBClassifier(use_label_encoder = False, eval_metrics = 'mlogloss', random_state = 42, tree_method='hist', device='cuda')
+
 
 param_grid={
     'learning_rate':[0.01,0.03,0.05,0.1,0.15,0.2],
@@ -32,20 +34,27 @@ param_grid={
     'subsample':[0.1,0.3,0.5,1]
 }
 
-grid_search = GridSearchCV(estimator=xgb, 
-                           param_grid=param_grid, 
-                           scoring='accuracy', 
-                           cv=3, 
-                           verbose=2,)
+random_search = RandomizedSearchCV(
+    estimator=xgb,
+    param_distributions=param_grid,
+    n_iter=50, 
+    scoring='accuracy',
+    cv=3,
+    verbose=2,
+    random_state=42,
+    n_jobs=1  # Keep at 1 when using GPU to avoid memory conflicts
+)
 
-print("Starting Hyperparameter Tuning...")
-grid_search.fit(X_train, y_train)
+print(f"Starting Randomized Search (Testing 50 of 1,440 combinations)...")
+random_search.fit(X_train, y_train)
 
-print(f"\nBest Parameters: {grid_search.best_params_}")
-print(f"Best Cross-Validation Score: {grid_search.best_score_:.4f}")
+# 4. Results
+print(f"\nBest Parameters found: {random_search.best_params_}")
+print(f"Best CV Score: {random_search.best_score_:.4f}")
 
-best_model = grid_search.best_estimator_
-predictions = best_model.predict(X_test)
 
-print("\nFinal Test Accuracy with Tuned Model:")
-print(accuracy_score(y_test, predictions))
+# 5. Evaluate the winner
+best_xgb = random_search.best_estimator_
+y_pred = best_xgb.predict(X_test)
+
+print(f"Final Test Accuracy: {accuracy_score(y_test, y_pred):.4f}")
